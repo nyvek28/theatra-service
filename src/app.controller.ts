de539cd972ee46398d11bcdbf4afddf8743cdcc7
Kevyn Quirós <kevynquiros@gmail.com>
2024-11-23 19:18:43 -0600

    feat: adds env variable support for deployment

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService
+  ) {
+    this.configService = configService
+  }
 
   @Get()
   getHello(): string {
@@ -13,5 +19,12 @@ export class AppController {
   @Get('ping')
   getPing(): string {
     return 'pang';
+  }
+
+  @Get('host')
+  getHost(): string {
+    const host = this.configService.get<string>('DB_HOST');
+    const res = host || 'nothing'
+    return res
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService) {
+    const loadedVariables = {
+      DB_HOST: configService.get<string>('DB_HOST'),
+      DB_NAME: configService.get<string>('DB_NAME'),
+      DB_PASSWORD: configService.get<string>('DB_PASSWORD'),
+      DB_PORT: configService.get<string>('DB_PORT'),
+      DB_USER: configService.get<string>('DB_USER'),
+    };
+
+    console.log('Loaded with variables:');
+    console.log(loadedVariables);
+  }
+}
